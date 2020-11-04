7aec37d49ebb95f2927581c8872f89cf5186f3a3
Felipe Donoso <felipedonoso@MacBook-Pro-de-Felipe.local>
2020-11-04 09:39:27 -0300

    Fin sección 3

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import PropType from "prop-types";
 
 const CounterApp = ({ value }) => {
-  const handleAdd = (event) => {
-    console.log(event);
+  //HOOKs https://es.reactjs.org/docs/hooks-intro.html
+  // Desesctructuramos el array del useState
+  const [counter, setCounter] = useState(value); // returnn []
+
+  const handleAdd = () => {
+    // setCounter( (c) => c + 1 )
+    setCounter(counter + 1);
+  };
+
+  const handleReset = () => {
+    setCounter(value);
   };
 
   return (
     <>
       <h1>CounterApp</h1>
-      <h2> Valor : {value} </h2>
+      <h2> Valor : {counter} </h2>
       <button onClick={handleAdd}> +1</button>
+      <button onClick={handleReset}> Reset</button>
+      <button onClick={() => setCounter(counter - 1)}> -1</button>
     </>
     // Eventos del botón
     // Docu: https://es.reactjs.org/docs/events.html
